a2a782ec8d81ec398a85da3f25a3b71c76de973c
Ahmed Radi <ahmedradi743@gmail.com>
2020-09-05 23:12:33 +0200

    Add include navbar and footer for pages

--- bouquet.php
+++ bouquet.php
@@ -8,89 +8,9 @@
 </head>
 
 <body class="no_col_2">
+
 <div id="site">
-    <div id="header" role="banner">
-        <h1 id="logo"><a href="index.php"><img src="images/logo.png" alt="Hansel and Petal" height="124" width="207"></a></h1>
-        <div class="inner">
-            <ul id="quick_links" class="reset menu">
-                <li><a href="#">My Account</a></li>
-                <li><a href="order.php">View My Order</a></li>
-                <li><a href="#">Customer Service</a></li>
-            </ul>
-            <form action="#" method="get" id="quick_search" role="search">
-                <input id="quickSearch" class="text white" placeholder="Find the perfect flowers or plants…" type="search">
-                <span class="btn_icon icon_search">
-                <input value="Search" type="submit">
-                </span>
-            </form>
-            <p id="offer">FREE Shipping on orders over $75.00!</p>
-        </div>
-    </div>
-    <div id="nav_main" role="navigation" class="reset menu pull_out">
-        <ul>
-            <li> <a href="arrangements.php" class="parent"><span>Arrangements</span></a>
-                <div>
-                    <ul>
-                        <li> <a href="#">Our Most Popular Flowers</a>
-                            <ul>
-                                <li><a href="#">Daisies</a></li>
-                                <li><a href="#">Tulips</a></li>
-                                <li><a href="#">Roses</a></li>
-                                <li><a href="#">Lilies</a></li>
-                                <li><a href="#">Irises</a></li>
-                                <li><a href="#">Mums</a></li>
-                                <li><a href="#">Carnations</a></li>
-                            </ul>
-                        </li>
-                        <li> <a href="#">Tropicals</a>
-                            <ul>
-                                <li><a href="#">Gingers</a></li>
-                                <li><a href="#">Heliconias</a></li>
-                                <li><a href="#">Tuberose</a></li>
-                                <li><a href="#">Ferns</a></li>
-                            </ul>
-                        </li>
-                        <li> <a href="#">Orchids</a>
-                            <ul>
-                                <li><a href="#">Phalaenopsis</a></li>
-                                <li><a href="#">Dendrobium</a></li>
-                                <li><a href="#">Oncidium</a></li>
-                                <li><a href="#">Cattleya</a></li>
-                                <li><a href="#">Cymbidium</a></li>
-                            </ul>
-                        </li>
-                        <li> <a href="mixed/mixed.php">Mixed Arrangements</a>
-                            <ul>
-                                <li><a href="#">Spring Pastels</a></li>
-                                <li><a href="#">Autumn Warmth</a></li>
-                                <li><a href="#">Red, White, &amp; Blue</a></li>
-                                <li><a href="#">All White</a></li>
-                                <li><a href="mixed/roses.php">Mixed Roses</a></li>
-                                <li><a href="#">Mixed Irises</a></li>
-                                <li><a href="#">Daisies &amp; More Daisies</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <li><a href="#"><span>Live Plants</span></a></li>
-            <li class="current"><a href="bouquet.php"><span>Build-a-Bouquet</span></a></li>
-            <li><a href="#"><span>Special Events</span></a></li>
-            <li> <a href="#" class="parent"><span>Care Tips</span></a>
-                <div class="single_column">
-                    <ul>
-                        <li><a href="#">Caring for Mixed Bouquets</a></li>
-                        <li><a href="#">Caring for Roses</a></li>
-                        <li><a href="#">Caring for Tropical Flowers</a></li>
-                        <li><a href="#">Caring for House Plants</a></li>
-                        <li><a href="care/orchids.php">Caring for Orchids</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li><a href="#"><span>Eco-Conscious</span></a></li>
-            <li><a href="designers.php"><span>Our Designers</span></a></li>
-        </ul>
-    </div>
+   <?php require 'includes/pagetop.php'; ?>
     <div id="content">
         <div id="breadcrumbs" class="reset menu">
             <ul>
@@ -330,41 +250,9 @@
             </form>
         </div>
     </div>
-    <div id="footer">
-        <div class="row clearfix">
-            <ul class="reset menu hover">
-                <li><a href="arrangements.php">Arrangements</a></li>
-                <li><a href="#">Live Plants</a></li>
-                <li><a href="bouquet.php">Build-a-Bouquet</a></li>
-                <li><a href="#">Special Events</a></li>
-                <li><a href="#">Care Tips</a></li>
-                <li><a href="#">Eco-Conscious</a></li>
-                <li><a href="designers.php">Our Designers</a></li>
-            </ul>
-            <ul class="reset menu hover">
-                <li><a href="#">My Account</a></li>
-                <li><a href="#">Order Status</a></li>
-                <li><a href="#">Customer Service</a></li>
-                <li><a href="#">Fresh Flower Guarantee</a></li>
-                <li><a href="#">Shipping Information</a></li>
-            </ul>
-            <ul class="reset menu hover">
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Terms &amp; Conditions</a></li>
-            </ul>
-            <img src="images/logo.png" alt="Hansel and Petal" height="124" width="207"> </div>
-        <p id="copyright" class="reset pull_out padding" role="contentinfo"><a href="http://www.ixd.hanselandpetal.com/">© 2013 Hansel and Petal</a></p>
-    <p id="disclaimer">Hansel &amp; Petal is a fictitious brand created by 
-        lynda.com solely for the purpose of training. All products and people 
-        associated with Hansel &amp; Petal are also fictitious. Any resemblance 
-        to real brands, products, or people is purely coincidental. Information 
-        provided about the product is also fictitious and should not be 
-        construed to be representative of actual products on the market in a 
-        similar product category.</p>
-    </div>
+<?php include('includes/footer.php') ?>
 </div>
-<script src="js/jquery-1.10.2.min.js"></script> 
+<script src="js/jquery-1.10.2.min.js"></script>
 <script src="js/scripts.js"></script>
 </body>
 </html>
